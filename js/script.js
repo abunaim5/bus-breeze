f743b3ac69e58586c6ab824632e49f2c7d350262
abunaim5 <abunaim140@gmail.com>
2024-02-19 02:03:19 +0600

    added more js functions for available seat, seat count, append table data

--- js/script.js
+++ js/script.js
@@ -3,3 +3,28 @@ function scrollSection() {
     const ticketBooking = getElementsById('ticket-booking');
     ticketBooking.scrollIntoView();
 }
+
+// click seat button function
+const seatButton = getElementsByClassName('.seat');
+const availableSeatText = getElementsInnerTextById('available-seat');
+let availableSeat = parseInt(availableSeatText);
+let totalPrice = 0;
+let grandTotal = 0;
+let seatCount = 0;
+for (const button of seatButton) {
+    button.addEventListener('click', function (e) {
+        const buttonId = button.innerText;
+        setElementsBgColorById(buttonId);
+        setElementsAttributeById(buttonId, 'disabled');
+
+        availableSeat = availableSeat - 1;
+        setElementsInnerTextById('available-seat', availableSeat);
+
+        seatCount += 1;
+        setElementsInnerTextById('seat-count', seatCount);
+
+        const ticketPriceText = getElementsInnerTextById('ticket-price');
+        const ticketPrice = parseFloat(ticketPriceText);
+        appendTableDataById('tbody', buttonId, 'Economy', ticketPriceText);
+    })
+}
--- js/utility.js
+++ js/utility.js
@@ -3,3 +3,56 @@ function getElementsById(elementId) {
     const element = document.getElementById(elementId);
     return element;
 }
+
+// get elements by class name
+function getElementsByClassName(className) {
+    const element = document.querySelectorAll(className);
+    return element;
+}
+
+// get elements inner text by id
+function getElementsInnerTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
+// set elements inner text by id
+function setElementsInnerTextById(elementId, text) {
+    const element = document.getElementById(elementId);
+    element.innerText = text;
+}
+
+// set elements background color by id
+function setElementsBgColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.style.backgroundColor = '#1DD100';
+}
+
+// set elements attribute by id
+function setElementsAttributeById(elementId, attributeName) {
+    const element = document.getElementById(elementId);
+    element.setAttribute(attributeName, true);
+}
+
+// remove elements attribute by id
+function removeElementsAttributeById(elementId, attributeName) {
+    const element = document.getElementById(elementId);
+    element.removeAttribute(attributeName);
+}
+
+// append table data by element id
+function appendTableDataById(elementId, td1, td2, td3) {
+    const element = document.getElementById(elementId);
+    const newRow = document.createElement('tr');
+    const newData1 = document.createElement('td');
+    const newData2 = document.createElement('td');
+    const newData3 = document.createElement('td');
+    newData1.appendChild(document.createTextNode(td1));
+    newData2.appendChild(document.createTextNode(td2));
+    newData3.appendChild(document.createTextNode(td3));
+    newRow.appendChild(newData1);
+    newRow.appendChild(newData2);
+    newRow.appendChild(newData3);
+    element.appendChild(newRow);
+}
